b6c973e8028c1cf9448285da4877537d3df151b1
Aditi Bhadoriya <awdhesh1700@gmail.com>
2024-06-09 01:48:08 +0530

    Homepage and sidebar added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,10 +5,12 @@ import {Routes, Route} from "react-router-dom"
 import SignUpPage from './pages/auth/signup/SignUpPage'
 import LoginPage from './pages/auth/login/LoginPage'
 import HomePage from './pages/home/HomePage'
+import Sidebar from './components/common/Sidebar'
 function App() {
 
   return (
     <div className="flex max-w-6xl mx-auto">
+      <Sidebar />
     <Routes>
       <Route path="/" element={<HomePage />}/>
       <Route path="/signup" element={<SignUpPage />}/>
