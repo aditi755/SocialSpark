72e283aaaea71cbeb05b837a22b96e3415e8c992
Aditi Bhadoriya <awdhesh1700@gmail.com>
2024-06-10 22:53:59 +0530

    profile and notification UI added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,8 @@ import LoginPage from './pages/auth/login/LoginPage'
 import HomePage from './pages/home/HomePage'
 import Sidebar from './components/common/Sidebar'
 import RightPanel from './components/common/RightPanel'
+import NotificationPage from './pages/notification/NotificationPage'
+import ProfilePage from './pages/profile/ProfilePage'
 function App() {
 
   return (
@@ -16,6 +18,8 @@ function App() {
       <Route path="/" element={<HomePage />}/>
       <Route path="/signup" element={<SignUpPage />}/>
       <Route path="/login" element={<LoginPage />}/>
+      <Route path="/notifications" element={<NotificationPage />}/>
+      <Route path="/profile/:username" element={<ProfilePage/>}/>
     </Routes>
     <RightPanel/>
     </div>
