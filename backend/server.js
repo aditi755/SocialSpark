27b30a7835dc854b4b8dc8a2b6e43285db0ac6d6
Aditi Bhadoriya <awdhesh1700@gmail.com>
2024-07-10 20:09:19 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,16 @@ import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser"
 import { v2 as cloudinary } from "cloudinary"
 import path from "path"
+import cors from 'cors';
+
+
 // import { fileURLToPath } from 'url';
 dotenv.config()
-
+app.use(cors,({
+    origin: ["https://twitter-clone-frontend-roan.vercel.app/"],
+    methods: ["POST", "GET", "POST", "PUT"],
+    credentials: true
+}))
 // // Get the directory name of the current module
 // const __filename = fileURLToPath(import.meta.url);
 // const __dirname = path.dirname(__filename);
