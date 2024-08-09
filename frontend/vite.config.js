1aaeda641d0c33e77915657821e517832791ebdd
Aditi Bhadoriya <awdhesh1700@gmail.com>
2024-08-09 13:55:52 +0530

    rem vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,16 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000,
+//     proxy: {
+//       "/api": {
+//         // target: "https://twitter-clone-liard-three.vercel.app",
+//         target: 'http://localhost:5000',
+//         changeOrigin: true
+//       }
+//     }
+//   }
+// })
+
+
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      "/api": {
-        // target: "https://twitter-clone-liard-three.vercel.app",
-        target: 'http://localhost:5000',
-        changeOrigin: true
-      }
-    }
-  }
-})
+	plugins: [react()],
+});
